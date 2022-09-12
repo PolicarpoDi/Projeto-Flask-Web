aaf59e6dea96e8afcc0fd901a9aa6d65f8d22978
Diego <policarpo16@yahoo.com.br>
2022-09-12 12:32:20 -0300

    Add signup

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
-
-@app.route('/home/<int:id>')
+"""@app.route('/home/<int:id>')
 def id(id):
     valor = id ** 2
-    return render_template('home.html', valor=valor)
+    return render_template('home.html', valor=valor)"""
 
-
-@app.route('/login/name/<string:nome>')
+"""@app.route('/login/name/<string:nome>')
 def login_name(nome):
-    return f'<h1>O nome desse usuário é {nome}</h1>'
+    return f'<h1>O nome desse usuário é {nome}</h1>'"""
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        # obter email e senha para cadastrar usuário
+        email = request.form['email_usuario']
+        senha = request.form['senha_usuario']
+        
+        if not (email or senha):
+            return render_template('signup.html')
+        
+        usuario = {'email': email}
+        
+        return render_template('signup_ok.html', usuario=usuario) 
+
+
 
 
 if __name__ ==  '__main__':
